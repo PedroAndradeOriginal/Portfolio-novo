34344c44cf8675e3215d41bab747fab5ac5eef1e
Pedro Andrade <pedrofreitasdeandrade@gmail.com>
2022-04-08 19:14:41 -0300

    Subir arquivos

---
+++ configScroll.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal({
+    distance: '50px',
+    duration: 800,
+    origin: 'top',
+    reset: false
+})
+
+
+// Delay
+
+sr.reveal('.d-200', { delay: 200 })
+sr.reveal('.d-300', { delay: 300 })
+sr.reveal('.d-400', { delay: 400 })
